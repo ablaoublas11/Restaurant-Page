5fba948181808a652df671f72ed49ca7e5cae8cc
ablaoublas11 <xristosxalkidis.exnet@gmail.com>
2026-07-26 14:40:14 +0300

    Menu.js updated to include food's category and price. Template.html updated to include a logo section in the header, a main section with a welcome message, and a contact button in the menu.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,182 +1,220 @@
 export const greekDishes = [
   {
     name: "Μουσακάς",
-    description: "Στρώσεις πατάτας, μελιτζάνας και κιμά με μπεσαμέλ, ψημένος στο φούρνο.",
-    //image: "./assets/images/moussakas.jpg",
-    price: 9.50
+    description:
+      "Στρώσεις πατάτας, μελιτζάνας και κιμά με μπεσαμέλ, ψημένος στο φούρνο.",
+    image: "images/moussakas.jpg",
+    price: 9.5,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Παστίτσιο",
     description: "Μακαρόνια με κιμά και μπεσαμέλ, ψημένα στο φούρνο.",
-    //image: "./assets/images/pastitsio.jpg",
-    price: 8.90
+    image: "images/pastitsio.jpg",
+    price: 8.9,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Σουβλάκι Χοιρινό",
     description: "Ψητό χοιρινό κρέας σε καλαμάκι, σερβιρισμένο με πίτα.",
-    //image: "./assets/images/souvlaki-xoirino.jpg",
-    price: 3.50
+    image: "images/souvlaki-xoirino.jpg",
+    price: 3.5,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Γύρος Κοτόπουλο",
-    description: "Ψητό κοτόπουλο σε πίτα με πατάτες, ντομάτα, κρεμμύδι και τζατζίκι.",
-    //image: "./assets/images/gyros-kotopoulo.jpg",
-    price: 3.80
+    description:
+      "Ψητό κοτόπουλο σε πίτα με πατάτες, ντομάτα, κρεμμύδι και τζατζίκι.",
+    image: "images/gyros-kotopoulo.jpg",
+    price: 3.8,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Τζατζίκι",
     description: "Κρεμώδης σάλτσα από γιαούρτι, αγγούρι, σκόρδο και ελαιόλαδο.",
-    //image: "./assets/images/tzatziki.jpg",
-    price: 3.00
+    image: "images/tzatziki.jpg",
+    price: 3.0,
+    category: "Ορεκτικό",
   },
   {
     name: "Ντολμαδάκια Γιαλαντζί",
-    description: "Αμπελόφυλλα γεμιστά με ρύζι και μυρωδικά, σερβιρισμένα με λεμόνι.",
-    //image: "./assets/images/dolmadakia.jpg",
-    price: 6.50
+    description:
+      "Αμπελόφυλλα γεμιστά με ρύζι και μυρωδικά, σερβιρισμένα με λεμόνι.",
+    image: "images/dolmadakia.jpg",
+    price: 6.5,
+    category: "Ορεκτικό",
   },
   {
     name: "Σπανακόπιτα",
     description: "Φύλλο κρούστας γεμιστό με σπανάκι και φέτα.",
-    //image: "./assets/images/spanakopita.jpg",
-    price: 3.20
+    image: "images/spanakopita.jpg",
+    price: 3.2,
+    category: "Πίτα",
   },
   {
     name: "Τυρόπιτα",
     description: "Φύλλο κρούστας γεμιστό με μείγμα τυριών.",
-    //image: "./assets/images/tyropita.jpg",
-    price: 3.00
+    image: "images/tyropita.jpg",
+    price: 3.0,
+    category: "Πίτα",
   },
   {
     name: "Γεμιστά",
-    description: "Ντομάτες και πιπεριές γεμιστές με ρύζι και μυρωδικά, ψημένες στο φούρνο.",
-    //image: "./assets/images/gemista.jpg",
-    price: 7.50
+    description:
+      "Ντομάτες και πιπεριές γεμιστές με ρύζι και μυρωδικά, ψημένες στο φούρνο.",
+    image: "images/gemista.jpg",
+    price: 7.5,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Φασολάδα",
     description: "Παραδοσιακή σούπα φασολιών με λαχανικά και ελαιόλαδο.",
-    //image: "./assets/images/fasolada.jpg",
-    price: 5.50
+    image: "images/fasolada.jpg",
+    price: 5.5,
+    category: "Σούπα",
   },
   {
     name: "Χωριάτικη Σαλάτα",
-    description: "Ντομάτα, αγγούρι, κρεμμύδι, πιπεριά, ελιές και φέτα με ελαιόλαδο.",
-    //image: "./assets/images/xoriatiki.jpg",
-    price: 6.00
+    description:
+      "Ντομάτα, αγγούρι, κρεμμύδι, πιπεριά, ελιές και φέτα με ελαιόλαδο.",
+    image: "images/xoriatiki.jpg",
+    price: 6.0,
+    category: "Σαλάτα",
   },
   {
     name: "Μελιτζανοσαλάτα",
     description: "Πουρές από ψητή μελιτζάνα με σκόρδο, ελαιόλαδο και λεμόνι.",
-    //image: "./assets/images/melitzanosalata.jpg",
-    price: 4.00
+    image: "images/melitzanosalata.jpg",
+    price: 4.0,
+    category: "Ορεκτικό",
   },
   {
     name: "Κεφτεδάκια",
     description: "Τηγανητοί μπιφτεκάκια με κιμά, ψωμί και μυρωδικά.",
-    //image: "./assets/images/keftedes.jpg",
-    price: 6.80
+    image: "images/keftedes.jpg",
+    price: 6.8,
+    category: "Ορεκτικό",
   },
   {
     name: "Μπριάμ",
     description: "Ψητά ανάμεικτα λαχανικά με ελαιόλαδο και μυρωδικά.",
-    //image: "./assets/images/briam.jpg",
-    price: 6.00
+    image: "images/briam.jpg",
+    price: 6.0,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Φάβα Σαντορίνης",
-    description: "Πουρές από κίτρινα αρακά, σερβιρισμένος με κρεμμύδι και ελαιόλαδο.",
-    //image: "./assets/images/fava.jpg",
-    price: 5.50
+    description:
+      "Πουρές από κίτρινα αρακά, σερβιρισμένος με κρεμμύδι και ελαιόλαδο.",
+    image: "images/fava.jpg",
+    price: 5.5,
+    category: "Ορεκτικό",
   },
   {
     name: "Χταποδάκι Σχάρας",
     description: "Ψητό χταπόδι στη σχάρα με λαδολέμονο.",
-    //image: "./assets/images/xtapodi.jpg",
-    price: 12.00
+    image: "images/xtapodi.jpg",
+    price: 12.0,
+    category: "Θαλασσινά",
   },
   {
     name: "Καλαμαράκια Τηγανιτά",
-    description: "Καλαμάρια κομμένα σε ροδέλες, τηγανισμένα σε τραγανό κουρκούτι.",
-    //image: "./assets/images/kalamarakia.jpg",
-    price: 9.00
+    description:
+      "Καλαμάρια κομμένα σε ροδέλες, τηγανισμένα σε τραγανό κουρκούτι.",
+    image: "images/kalamarakia.jpg",
+    price: 9.0,
+    category: "Θαλασσινά",
   },
   {
     name: "Κοτόπουλο Λεμονάτο",
     description: "Ψητό κοτόπουλο με πατάτες, λεμόνι, ρίγανη και ελαιόλαδο.",
-    //image: "./assets/images/kotopoulo-lemonato.jpg",
-    price: 8.50
+    image: "images/kotopoulo-lemonato.jpg",
+    price: 8.5,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Κοτόσουπα Αυγολέμονο",
     description: "Παραδοσιακή σούπα κοτόπουλου με ρύζι και αυγολέμονο.",
-    //image: "./assets/images/avgolemono.jpg",
-    price: 6.00
+    image: "images/avgolemono.jpg",
+    price: 6.0,
+    category: "Σούπα",
   },
   {
     name: "Λαχανοντολμάδες",
     description: "Λάχανο γεμιστό με κιμά και ρύζι, σε σάλτσα αυγολέμονο.",
-    //image: "./assets/images/lahanontolmades.jpg",
-    price: 7.00
+    image: "images/lahanontolmades.jpg",
+    price: 7.0,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Μπακαλιάρος Σκορδαλιά",
     description: "Τηγανητός μπακαλιάρος με σκορδαλιά πατάτας.",
-    //image: "./assets/images/bakaliaros-skordalia.jpg",
-    price: 8.50
+    image: "images/bakaliaros-skordalia.jpg",
+    price: 8.5,
+    category: "Θαλασσινά",
   },
   {
     name: "Γαρίδες Σαγανάκι",
     description: "Γαρίδες σοταρισμένες σε σάλτσα ντομάτας με φέτα.",
-    //image: "./assets/images/garides-saganaki.jpg",
-    price: 11.00
+    image: "images/garides-saganaki.jpg",
+    price: 11.0,
+    category: "Θαλασσινά",
   },
   {
     name: "Στιφάδο",
     description: "Μοσχάρι μαγειρεμένο αργά με κρεμμύδια και κόκκινη σάλτσα.",
-    //image: "./assets/images/stifado.jpg",
-    price: 10.50
+    image: "images/stifado.jpg",
+    price: 10.5,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Σουτζουκάκια",
     description: "Επιμήκη κεφτεδάκια σε πικάντικη σάλτσα ντομάτας με κύμινο.",
-    //image: "./assets/images/soutzoukakia.jpg",
-    price: 8.00
+    image: "images/soutzoukakia.jpg",
+    price: 8.0,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Μπιφτέκια Σχάρας",
     description: "Ψητά μπιφτέκια με κιμά, κρεμμύδι και μυρωδικά.",
-    //image: "./assets/images/biftekia.jpg",
-    price: 7.80
+    image: "images/biftekia.jpg",
+    price: 7.8,
+    category: "Κυρίως Πιάτο",
   },
   {
     name: "Χαλβάς",
     description: "Παραδοσιακό γλυκό από σιμιγδάλι, ξηρούς καρπούς και σιρόπι.",
-    //image: "./assets/images/halvas.jpg",
-    price: 3.50
+    image: "images/halvas.jpg",
+    price: 3.5,
+    category: "Γλυκό",
   },
   {
     name: "Μπακλαβάς",
     description: "Φύλλο κρούστας με ξηρούς καρπούς και σιρόπι μελιού.",
-    //image: "./assets/images/baklavas.jpg",
-    price: 4.00
+    image: "images/baklavas.jpg",
+    price: 4.0,
+    category: "Γλυκό",
   },
   {
     name: "Γαλακτομπούρεκο",
     description: "Κρέμα σιμιγδαλιού τυλιγμένη σε φύλλο, με σιρόπι.",
-    //image: "./assets/images/galaktoboureko.jpg",
-    price: 4.20
+    image: "images/galaktoboureko.jpg",
+    price: 4.2,
+    category: "Γλυκό",
   },
   {
     name: "Λουκουμάδες",
     description: "Τηγανητά ζυμαράκια με μέλι και κανέλα.",
-    //image: "./assets/images/loukoumades.jpg",
-    price: 4.50
+    image: "images/loukoumades.jpg",
+    price: 4.5,
+    category: "Γλυκό",
   },
   {
     name: "Κανταΐφι",
-    description: "Σιροπιαστό γλυκό από λεπτές νηματοειδείς ζύμες με ξηρούς καρπούς.",
-    //image: "./assets/images/kantaifi.jpg",
-    price: 4.00
-  }
+    description:
+      "Σιροπιαστό γλυκό από λεπτές νηματοειδείς ζύμες με ξηρούς καρπούς.",
+    image: "images/kantaifi.jpg",
+    price: 4.0,
+    category: "Γλυκό",
+  },
 ];
