6f2cddf929f9b92e3f834b32761d43747698befc
ablaoublas11 <50484255+ablaoublas11@users.noreply.github.com>
2026-07-26 19:59:45 +0000

    Javascript template and basic methods for menu buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
 import "./style.css";
 
-import {greekDishes} from "./scripts/menu";
+// import {greekDishes} from "./scripts/menu";
 
-console.log(greekDishes);
+// console.log(greekDishes);
+
+const app = {
+    elements:{
+        navMenu: document.querySelector(".header .nav .menu")
+    },
+    init(){
+        //προσθήκη eventListener σε όλα τα κουμπιά του menu
+        this.elements.navMenu.addEventListener("click" , (event) => {
+            const target = event.target.dataset.view;
+            if(!target){
+                return;
+            }else if(target==="home"){
+                this.renderHome();
+            }
+            else if(target==="menu"){
+                this.renderMenu();
+            }
+            else if(target==="about"){
+                this.renderAbout();
+            }
+            else if(target==="contact"){
+                this.renderContact();
+            }
+
+        });
+    },
+    renderHome(){
+        alert("hi home");
+    },
+    renderMenu(){
+        alert("hi menu");
+    },
+    renderAbout(){
+        alert("hi about");
+    },
+    renderContact(){
+        alert("hi contact");
+    }
+}
+
+app.init();
