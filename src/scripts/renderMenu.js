23af7a6e87b62bac26d1364c646639fe9ba658d2
ablaoublas11 <50484255+ablaoublas11@users.noreply.github.com>
2026-07-30 11:06:45 +0000

    Update renderMenu.js

--- src/scripts/renderMenu.js
+++ src/scripts/renderMenu.js
@@ -3,23 +3,27 @@ import {greekDishes} from "./menu";
 export const renderMenu = () => {
     //αποθηκεύουμε τα πιάτα αφού τα έχουμε ομαδοποιήσει
     const groupedDishes = groupingDishes(greekDishes);
-    console.log(groupedDishes);
-
+    console.log(groupedDishes.length);
+    //Δημιουργία της βάσης του δέντρου του μενού
     const node = document.createElement("div");
     node.className = "menuContent";
 
-    //πέρασμα όλου του πίνακα και δημιουργία html tag για κάθε στοιχείο του πίνακα
-    greekDishes.forEach((element, index) => {
-        const childNode = document.createElement("div");
-        childNode.className = "menuItem";
-        const childItem = `
-            <h3>${element.name}</h3>
-            <h4>${element.category}</h4>
-            <p>${element.description}</p>
-            <p>${element.price}</p>
-        `;
-        childNode.insertAdjacentHTML("beforeend", childItem);
-        node.append(childNode);
+    Object.entries(groupedDishes).forEach(([categoryName, dishesArray]) => {
+        //alert("hi");
+        dishesArray.forEach((element) => {
+            const childNode = document.createElement("div");
+            childNode.className = "menuItem";
+
+            const childItem = `
+                <h3>${element.name}</h3>
+                <h4>${element.category}</h4>
+                <p>${element.description}</p>
+                <p>${element.price}</p>
+            `;
+            childNode.insertAdjacentHTML("beforeend", childItem);
+            node.append(childNode);
+            console.log(element);
+        });
     });
     return node;
 }
